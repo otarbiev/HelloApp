6ede831eebb362fbe4dfd10e7b27d3a0feda0f40
Elman <otarbiev.elman@gmail.com>
2023-07-19 20:45:52 +0300

    cube-update
    num в кубе

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Changes in \"new-feature\" branch");
+var num = 4;
+var numInCube = num * num;
+Console.WriteLine($"{num} в кубе = {numInCube}");
