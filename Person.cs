6e98be18ae1b2a37a0fec492b39e71638d064393
Elman <otarbiev.elman@gmail.com>
2023-07-19 20:55:01 +0300

    Update Person.cs
    Added constructor and method in the Person class

--- Person.cs
+++ Person.cs
@@ -8,5 +8,21 @@ namespace HelloApp
 {
     internal class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void SayName()
+        {
+            Console.WriteLine($"My name is {Name}");
+        }
+
+
+
     }
 }
