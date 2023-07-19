3f4c2ebb3883823d395e6114d9e23c16cb9911c5
Elman <otarbiev.elman@gmail.com>
2023-07-19 20:49:27 +0300

    user-input
    1. Дана возможность пользователю ввести число, которое нужно возвести в квадрат.
    2.Убраны var типы

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Changes in \"new-feature\" branch");
-var num = 4;
-var numInCube = num * num;
+
+Console.WriteLine("Введите число чтобы возвести его в куб: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int numInCube = num * num;
 Console.WriteLine($"{num} в кубе = {numInCube}");
